34cddf4e6100e6801ef02449c7ae01a64f4cfb11
Daniel_Blythe <daniel.blythe@2112comms.com>
2018-11-09 14:55:41 +0000

    put GET PUT back in headers

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(bodyParser.json());
 // set CORS headers on server as server listens on port 8080
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
