703f327c829044171259d1b3c3c7bc2334654fe4
Daniel_Blythe <daniel.blythe@2112comms.com>
2019-05-14 22:27:00 +0100

    removed comments in passport.js

--- config/passport.js
+++ config/passport.js
@@ -13,22 +13,13 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = SECRET;
 
 module.exports = (passport) => {
-    // console.log('opts.jwtFromRequest: ', opts.jwtFromRequest);
-    
     passport.use(
         new JwtStrategy(opts, (jwt_payload, done) => {
-            // console.log('step 1', jwt_payload.id);
-            
             User.findById(jwt_payload.id)
                 .then(user => {
-                    // console.log('founder user: ', user);
-                    
                     if (user) {
-                        console.log('User found and Auth success');
-                        
                         return done(null, user);
                     }
-                    console.log('User NOT found and Auth failed');
                     return done(null, false);
                 })
                 .catch(err => console.log(err));
