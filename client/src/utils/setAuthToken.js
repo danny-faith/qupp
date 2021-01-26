2739ef0de9ddbd9c0b1a6ca1fe9b4f823d64e31a
Daniel Blythe <d.blythe@explori.com>
2021-01-26 17:58:17 +0000

    converting getToken to ramda WIP

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+import { ifElse, type, both, is, not, tap } from 'ramda'
 
-const setAuthToken = (token) => {
-    if (token) {
-        // Apply token to every request
-        axios.defaults.headers.common['Authorization'] = token;
-    } else {
-        // Delete/make sure no token is in header is token if no token exists
-        delete axios.defaults.headers.common['Authorization'];
-    }
-}
+/**
+ * use lensProp to get inside axios
+ */
+
+const setAuthToken = ifElse(
+    is(String),
+    (token) => axios.defaults.headers.common['Authorization'] = token,
+    () => delete axios.defaults.headers.common['Authorization'],
+)
 
 export default setAuthToken;
--- client/src/utils/setAuthToken.test.js
+++ client/src/utils/setAuthToken.test.js
@@ -1,2 +1,31 @@
+import axios from 'axios'
 import setAuthToken from './setAuthToken'
 
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZGUwOWNiMzQ1YzY2MzNkNzdhOWIwNiIsInVzZXJuYW1lIjoiRGFubnlXb2JibGUzIiwiYXZhdGFyIjoidXBsb2Fkcy9hdmF0YXJzLzIwMjAtMTItMjBUMTg6NDI6MDUuMDg0Wi1wbmctdHJhbnNwYXJlbnQtY29sb25lbC1zYW5kZXJzLWtmYy1mcmllZC1jaGlja2VuLWZhc3QtZm9vZC1yZXN0YXVyYW50LWJlYXV0eS1wYXJsb3Itcy1mb29kLWZhc3QtZm9vZC1yZXN0YXVyYW50LWxvZ28ucG5nIiwiaWF0IjoxNjExNjc2NjA2LCJleHAiOjE2MTE2ODAyMDZ9.Ck1MFnaNY425LKUB8inUKe3sLNpxB3nKgfOKi9kfvj0'
+
+describe('Testing setAuthToken', () => {
+    beforeEach(() => {
+        return delete axios.defaults.headers.common['Authorization'];
+    });
+
+    it('Token supplied', () => {
+        setAuthToken(token)
+        expect(axios.defaults.headers.common['Authorization']).toBe(token);
+    });
+    
+    it('No token supplied', () => {
+        setAuthToken()
+        expect(axios.defaults.headers.common['Authorization']).toBe(undefined);
+    });
+    
+    it('Invalid token type supplied: number, object, array and NaN', () => {
+        setAuthToken(12)
+        expect(axios.defaults.headers.common['Authorization']).toBe(undefined);
+        setAuthToken({})
+        expect(axios.defaults.headers.common['Authorization']).toBe(undefined);
+        setAuthToken([])
+        expect(axios.defaults.headers.common['Authorization']).toBe(undefined);
+        setAuthToken(NaN)
+        expect(axios.defaults.headers.common['Authorization']).toBe(undefined);
+    });
+})
