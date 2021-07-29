2db1efc35145930d9ba9e3bdd136984a34ee5a49
Daniel Blythe <d.blythe@explori.com>
2021-07-29 20:33:59 +0100

    converted editQupplist to func comp + slight refactor

--- client/src/pages/EditQupplist.js
+++ client/src/pages/EditQupplist.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import CreatePlaylist from '../components/playlist/CreatePlaylist';
-import { getPlaylist } from '../actions/playlistActions';
-import isEmpty from '../utils/isEmpty';
+import React, { useEffect, useState, useMemo, useLayoutEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import CreatePlaylist from '../components/playlist/CreatePlaylist'
+import { getPlaylist } from '../actions/playlistActions'
+import isEmpty from '../utils/isEmpty'
 
-class EditPlaylist extends Component {
-    componentDidMount = () => {
-        const { isAuthenticated } = this.props.auth
+function EditPlaylist(props) {
+    const [playlist, setPlaylist] = useState({})
+    const [errors, setErrors] = useState([])
+
+    useEffect(() => {
+        const { isAuthenticated } = props.auth
+    
+        if (!isAuthenticated) {
+            props.history.push('/login')
+        }
+    }, [])
+
+    useLayoutEffect(() => {
+        props.getPlaylist(props.match.params.slug)
+    }, [])
+
+    useMemo(() => {
+        const { isAuthenticated } = props.auth
 
         if (!isAuthenticated) {
-            this.props.history.push('/login');
+            props.history.push('/login')
         }
-    }
-
-    componentWillMount = () => {
-        this.props.getPlaylist(this.props.match.params.slug);
-    }
-
-    componentWillReceiveProps = (nextProps) => {
-        const { isAuthenticated } = nextProps.auth
-
-        if (!isAuthenticated) {
-            this.props.history.push('/login');
+        if (props.errors) {
+            setErrors(props.errors)
         }
-        if (nextProps.errors) {
-            this.setState({
-                errors: nextProps.errors,
-            });
+        if (props.playlist) {
+            setPlaylist(props.playlist)
         }
-        if (nextProps.playlist) {
-            this.setState(nextProps.playlist);
-        }
-    }
+    }, [props.errors, props.playlist, props.auth])
 
-    editQupplistContent = () => {
-        const playlistsHaveLoaded = !isEmpty(this.state)
+    const editQupplistContent = () => {
+        const playlistsHaveLoaded = !isEmpty(playlist.playlist)
 
-        if(playlistsHaveLoaded) {
+        if (playlistsHaveLoaded) {
             return (
                 <CreatePlaylist
-                    name={this.state.playlist.name}
-                    slug={this.props.match.params.slug}
-                    id={this.state.playlist._id}
+                    name={playlist.playlist.name}
+                    slug={playlist.playlist.slug}
+                    id={playlist.playlist._id}
                     title="Edit playlist"
                     buttonText="Edit playlist"
                 />
@@ -50,14 +51,13 @@ class EditPlaylist extends Component {
         }
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Edit playlist</h1>
-                {this.editQupplistContent()}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h1>Edit playlist</h1>
+            {errors}
+            {editQupplistContent()}
+        </div>
+    )
 }
 
 EditPlaylist.propTypes = {
@@ -69,6 +69,6 @@ EditPlaylist.propTypes = {
 const mapStateToProps = (state) => ({
   auth: state.auth,
   playlist: state.playlist
-});
+})
 
-export default connect(mapStateToProps, { getPlaylist })(EditPlaylist);
+export default connect(mapStateToProps, { getPlaylist })(EditPlaylist)
