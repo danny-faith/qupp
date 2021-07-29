c046e5afee6a8be18acf0909b1dea3ae4de76618
Daniel Blythe <d.blythe@explori.com>
2021-07-29 08:56:04 +0100

    Song comp converted to functional comp

--- client/src/components/playlist/Song.js
+++ client/src/components/playlist/Song.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { Row, Col, Button } from 'react-materialize';
 
-export default class Song extends Component {
-    componentDidMount = () => {
-        const elems = document.querySelectorAll('.fixed-action-btn')
-        window.M.FloatingActionButton.init(elems, {
-            direction: 'left'
-        });
-    }
+function Song(props) {
+    const { type, colour, data } = props
 
-    handleAddSong = (e) => {    
-        const { data } = this.props;
+    const handleAddSong = (e) => {    
+        const { data } = props;
+        const { type } = e.currentTarget.dataset
         const songToAdd = {
             name: data.name,
             album: data.album,
@@ -20,14 +16,15 @@ export default class Song extends Component {
             duration_ms: data.duration_ms,
             uri: data.uri
         }
-        this.props.addSongToQueueOrQupplistHandler(songToAdd, e.currentTarget.dataset.type)
+        
+        props.addSongToQueueOrQupplistHandler(songToAdd, type)
     }
 
-    handleRemoveSong = (e) => {
-        this.props.removeSongFromQueueOrPlaylist(this.props.index, e.currentTarget.dataset.type)
+    const handleRemoveSong = (e) => {
+        props.removeSongFromQueueOrPlaylist(props.index, e.currentTarget.dataset.type)
     }
 
-    createActionButton = ({ dataType, classNames, icon, action }) => (
+    const createActionButton = ({ dataType, classNames, icon, action }) => (
         <Button
             onClick={action}
             data-type={dataType}
@@ -36,55 +33,53 @@ export default class Song extends Component {
         />
     )
 
-    buttonDecider = (type) => {
+    const buttonDecider = (type) => {
         switch (type) {
             case 'search':
                 return (
-                    <Button ref={this.myRef} floating fab='horizontal' icon='more_horiz' className='pink' large style={{bottom: '0px', right: '0px'}}>
-                        {this.createActionButton({ dataType: 'qupplist', icon: 'playlist_add', classNames: 'blue', action: this.handleAddSong })}
-                        {this.createActionButton({ dataType: 'queue', icon: 'playlist_add', classNames: 'yellow darken-1', action: this.handleAddSong })}
-                    </Button>
+                    <Fragment>
+                        {createActionButton({ dataType: 'qupplist', icon: 'playlist_add', classNames: 'blue', action: handleAddSong })}
+                        {createActionButton({ dataType: 'queue', icon: 'playlist_add', classNames: 'yellow darken-1', action: handleAddSong })}
+                    </Fragment>
                 )
             case 'qupplist':
                 return (
-                <Button ref={this.myRef} floating fab='horizontal' icon='more_horiz' className='pink' large style={{bottom: '0px', right: '0px'}}>
-                    {this.createActionButton({ dataType: 'qupplist', icon: 'delete', classNames: 'red darken-1', action: this.handleRemoveSong })}
-                    {this.createActionButton({ dataType: 'queue', icon: 'playlist_add', classNames: 'yellow darken-1', action: this.handleAddSong })}
-                </Button>
+                <Fragment>
+                    {createActionButton({ dataType: 'qupplist', icon: 'delete', classNames: 'red darken-1', action: handleRemoveSong })}
+                    {createActionButton({ dataType: 'queue', icon: 'playlist_add', classNames: 'yellow darken-1', action: handleAddSong })}
+                </Fragment>
             )
             case 'queue':
                 return (
-                <Button ref={this.myRef} floating fab='horizontal' icon='more_horiz' className='pink' large style={{bottom: '0px', right: '0px'}}>
-                    {this.createActionButton({ dataType: 'queue', icon: 'delete', classNames: 'red darken-1', action: this.handleRemoveSong })}
-                    {this.createActionButton({ dataType: 'qupplist', icon: 'playlist_add', classNames: 'blue darken-1', action: this.handleAddSong })}
-                </Button>
+                <Fragment>
+                    {createActionButton({ dataType: 'queue', icon: 'delete', classNames: 'red darken-1', action: handleRemoveSong })}
+                    {createActionButton({ dataType: 'qupplist', icon: 'playlist_add', classNames: 'blue darken-1', action: handleAddSong })}
+                </Fragment>
             )
             default:
                 break
         }
     }
-            
-            
-    render () {
-        const { type, colour, data } = this.props
-        const songButtons = this.buttonDecider(type)
 
-        return (
-            <Row className={`${colour} py-3 darken-2 mb-0`}>
-                <Col s={2} className="">
-                    <img alt="Song cover" src={data.image} className="w-full block" />
-                </Col>
-                <Col s={8} className="pl-0">
-                    <p className="my-0">{data.name}, {data.album}, Artists: 
-                        {data.artists.map(key => (
-                            <span key={key.name}> {key.name}</span>
-                        ))}
-                    </p>
-                </Col>
-                <Col s={2} className="pl-0 relative">
-                    {songButtons}
-                </Col>
-            </Row>
-        )
-    }
+    const songButtons = buttonDecider(type)
+
+    return (
+        <Row className={`${colour} py-3 darken-2 mb-0`}>
+            <Col s={2}>
+                <img alt="Song cover" src={data.image} className="w-full block" />
+            </Col>
+            <Col s={8} className="pl-0">
+                <p className="my-0">{data.name}, {data.album}, Artists: 
+                    {data.artists.map(key => (
+                        <span key={key.name}> {key.name}</span>
+                    ))}
+                </p>
+            </Col>
+            <Col s={2} className="pl-0 relative">
+                {songButtons}
+            </Col>
+        </Row>
+    )
 }
+
+export default Song
